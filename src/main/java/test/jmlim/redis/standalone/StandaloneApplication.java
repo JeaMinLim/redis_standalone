7d2acc48b53c25a71ceaf92f36d52367d20f94b7
JeaMinLim <gam1532@gmail.com>
2022-05-27 07:15:37 +0900

    The first commit

---
+++ src/main/java/test/jmlim/redis/standalone/StandaloneApplication.java
@@ -0,0 +1,13 @@
+package test.jmlim.redis.standalone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandaloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandaloneApplication.class, args);
+    }
+
+}
