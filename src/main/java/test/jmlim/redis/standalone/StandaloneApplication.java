1de4bae42371a8a4ee0c50b3dc5eda4629174673
JeaMinLim <gam1532@gmail.com>
2022-05-27 10:45:45 +0900

    update application.yaml

--- src/main/java/test/jmlim/redis/standalone/StandaloneApplication.java
+++ src/main/java/test/jmlim/redis/standalone/StandaloneApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StandaloneApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StandaloneApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(StandaloneApplication.class, args);
+	}
 
 }
